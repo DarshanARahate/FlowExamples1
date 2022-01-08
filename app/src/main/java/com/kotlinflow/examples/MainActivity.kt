c06dfa5796763a91d301ea5afcbc7d8b67b2529a
darshanrahate <rahate.darshan@gmail.com>
2022-01-09 01:15:57 +0530

    Filter operator is used.

--- app/src/main/java/com/kotlinflow/examples/MainActivity.kt
+++ app/src/main/java/com/kotlinflow/examples/MainActivity.kt
@@ -3,20 +3,18 @@ package com.kotlinflow.examples
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
-//import com.kotlinflow.examples.ex1.Example1
-//import com.kotlinflow.examples.ex1.Example2
-//import com.kotlinflow.examples.ex1.Example3
-import com.kotlinflow.examples.ex1.Example4
+import com.kotlinflow.examples.ex1.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Example1.startExample()
+//        Example1.startExample()
 //        Example2.startExample()
 //        Example3.startExample()
-        Example4.startExample(lifecycleScope)
+//        Example4.startExample(lifecycleScope)
+        Example5.startExample(lifecycleScope)
 
 
     }
---
+++ app/src/main/java/com/kotlinflow/examples/ex1/Example5.kt
@@ -0,0 +1,45 @@
+package com.kotlinflow.examples.ex1
+
+import androidx.lifecycle.LifecycleCoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+object Example5 {
+
+
+    fun startExample(lifecycleScope: LifecycleCoroutineScope) {
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            simpleFlow()
+                .filter {
+                    it == "User 3"
+                }
+                .collect {
+                    println("User List : $it")
+                }
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            userIds.asFlow().filter {
+               it % 3 == 1
+            }.collect {
+                println("User ids : $it")
+            }
+        }
+    }
+
+    private fun simpleFlow() : Flow<String> = flow {
+        userList.forEach {
+            emit(it)
+            delay(5000)
+        }
+    }
+
+    val userList = listOf("User 1", "User 2", "User 3", "User 4", "User 5")
+    val userIds = listOf(10,20,30,40,50,60,70,80,90)
+
+
+
+}
